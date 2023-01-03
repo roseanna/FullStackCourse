06d650b5baba37b94dae9d34a3fb641ba9dceacf
Roseanna <roseanna.chu1@gmail.com>
2023-01-02 21:28:26 -0500

    completed part 1b

---
+++ part 1/courseinfo/part1_exercises/src/App.js
@@ -0,0 +1,60 @@
+const Header = (props) => (
+  <h1>{props.course}</h1>
+)
+
+const Part = (props) => (
+  <p>{props.part.name} {props.part.exercises}</p>
+)
+
+const Content = (props) => {
+  console.log(props)
+  const [part1, part2, part3] = props.parts
+  return (
+    <div> 
+      <Part part={part1}/>
+      <Part part={part2}/>
+      <Part part={part3}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  let total = 0
+  props.parts.forEach(part => {
+    total += part.exercises
+  });
+
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development', 
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </>    
+  )
+}
+
+export default App;
